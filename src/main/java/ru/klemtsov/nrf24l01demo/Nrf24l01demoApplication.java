0a4ee0819f3e74263c1fb4b16f063b620b10da38
oleg <klemtsov@gmail.com>
2018-01-29 23:04:17 +0300

    new

--- src/main/java/ru/klemtsov/nrf24l01demo/Nrf24l01demoApplication.java
+++ src/main/java/ru/klemtsov/nrf24l01demo/Nrf24l01demoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.ContextStartedEvent;
 import ru.klemtsov.nrf24l01demo.driver.NRF24L01;
 
@@ -14,7 +15,7 @@ public class Nrf24l01demoApplication {
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Nrf24l01demoApplication.class);
-        springApplication.addListeners((ApplicationListener<ContextStartedEvent>)event ->{
+        springApplication.addListeners((ApplicationListener<ContextRefreshedEvent>) event ->{
             nrf24l01Service = event.getApplicationContext().getBean(Nrf24l01Service.class);
             System.out.println("START nrf24L01 succeeded");
 
