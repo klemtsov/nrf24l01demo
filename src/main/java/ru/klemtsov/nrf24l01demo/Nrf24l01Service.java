020e0a91f26c8b03bce2cfa5bd0904225f38b77f
oleg <klemtsov@gmail.com>
2018-01-30 00:25:40 +0300

    new

--- src/main/java/ru/klemtsov/nrf24l01demo/Nrf24l01Service.java
+++ src/main/java/ru/klemtsov/nrf24l01demo/Nrf24l01Service.java
@@ -31,7 +31,6 @@ public class Nrf24l01Service {
                     nrf24L01.start();
                     started = true;
                 }
-                0x0000010400
                 int[] txaddr = new int[]{0, 0, 0x01, 0x04, 0};
                 int[] txdata = new int[]{1};
                 nrf24L01.send(122, 1, 10, 5, txaddr, 1, txdata);
