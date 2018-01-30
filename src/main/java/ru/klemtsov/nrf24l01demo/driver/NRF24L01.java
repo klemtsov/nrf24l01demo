b75c12d10586bb844b25ca71bb32db0848dd68d8
oleg <klemtsov@gmail.com>
2018-01-30 22:38:03 +0300

    new

--- src/main/java/ru/klemtsov/nrf24l01demo/driver/NRF24L01.java
+++ src/main/java/ru/klemtsov/nrf24l01demo/driver/NRF24L01.java
@@ -113,7 +113,8 @@ public class NRF24L01 implements IRegister, Runnable {
 	 * default local RF address which is one byte length n 5
 	 */
 	//private int[] localRFAddress={ 53, 69, 149, 231, 231 };
-	private int[] localRFAddress={ 0, 0, 0, 0, 0 };
+	private int[] localRFAddress={ 0, 0, 0, 0, 0x01 };
+
 	/**
 	 * one flag marking thread running
 	 */
